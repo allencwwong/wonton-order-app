7ff118dc468844d731f6b3d39f5dbd4d8e7beaa7
allen wong <allencwwong@verizonmedia.com>
2019-07-09 23:12:44 -0700

    updated ui for product menu

--- src/Components/Order/_styles/ProductsMenu.js
+++ src/Components/Order/_styles/ProductsMenu.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const CSSProductsMenuItem = styled.div`
     .menu-item {
         cursor: pointer;
-        border-color: ${(props) => (props.active ? 'green' : '')};
+        border-color: ${(props) => props.active && 'green'};
+        background-color: ${(props) => props.active && '#dbffe2'};
     }
     .product-icon {
         width: 100%;
